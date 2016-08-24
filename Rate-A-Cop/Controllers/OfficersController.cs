5f4afc2af39cbf67850144dd485791473158598a
Juan Granda <juanfromecuador@gmail.com>
2016-08-24 14:56:39 -0400

    deleted edit and delete methods in officers controller and index

--- Rate-A-Cop/Controllers/OfficersController.cs
+++ Rate-A-Cop/Controllers/OfficersController.cs
@@ -56,7 +56,6 @@ namespace Rate_A_Cop.Controllers
             }
 
             return View(officers);
-            //return View(db.Officers.ToList());
         }
 
         // GET: Officers/Details/5
@@ -95,63 +94,6 @@ namespace Rate_A_Cop.Controllers
             }
 
             return View(officer);
-        }
-
-        // GET: Officers/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Officer officer = db.Officers.Find(id);
-            if (officer == null)
-            {
-                return HttpNotFound();
-            }
-            return View(officer);
-        }
-
-        // POST: Officers/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "OfficerID,BadgeNumber,OfficerName")] Officer officer)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(officer).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            return View(officer);
-        }
-
-        // GET: Officers/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Officer officer = db.Officers.Find(id);
-            if (officer == null)
-            {
-                return HttpNotFound();
-            }
-            return View(officer);
-        }
-
-        // POST: Officers/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            Officer officer = db.Officers.Find(id);
-            db.Officers.Remove(officer);
-            db.SaveChanges();
-            return RedirectToAction("Index");
         }
 
         protected override void Dispose(bool disposing)
