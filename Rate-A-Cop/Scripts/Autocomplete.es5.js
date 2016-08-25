137e4a003bcfd9f3ba08d98ee63905bf6dae6ad4
Juan Granda <juanfromecuador@gmail.com>
2016-08-25 18:14:36 -0400

    EasyAutocomplete

---
+++ Rate-A-Cop/Scripts/Autocomplete.es5.js
@@ -0,0 +1,22 @@
+﻿"use strict";
+
+var options = {
+
+    url: "Officers.json",
+
+    getValue: "name",
+    //getValue: function(element) {
+    //    return element.name;
+    //},
+
+    list: {
+        match: {
+            enabled: true
+        }
+    },
+
+    theme: "square"
+};
+
+$("#officers-json").easyAutocomplete(options);
+
---
+++ Rate-A-Cop/Scripts/Autocomplete.js
@@ -0,0 +1,19 @@
+﻿var options = {
+
+    url: "Officers.json",
+
+    getValue: "name",
+    //getValue: function(element) {
+    //    return element.name;
+    //},
+
+    list: {
+        match: {
+            enabled: true
+        }
+    },
+
+    theme: "square"
+};
+
+$("#officers-json").easyAutocomplete(options);
